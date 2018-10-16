d919672c668084dd64ee8b0f39c428651b0fbfeb
Juneyoung Lee <aqjune@gmail.com>
2018-10-16 14:28:14 +0900

    Add stringmatch2d library

---
+++ stringmatch2d/src/naive.rs
@@ -0,0 +1,30 @@
+/*
+ * Naive implementation of 2-dimensional string match.
+ * Returns: result; a list of left-upper most coordinates in (y, x) form
+ */
+pub fn run(text_vec:&Vec<Vec<char>>, query_vec:&Vec<Vec<char>>,
+           result:&mut Vec<(usize, usize)>) {
+  let thei:usize = text_vec.len();
+  let twid:usize = text_vec[0].len(); // Assumes that there is at least a character in text_vec;
+  let qhei:usize = query_vec.len();
+  let qwid:usize = query_vec[0].len();
+
+  for i in 0..thei - qhei {
+    for j in 0..twid - qwid {
+      let mut found:bool = true;
+
+      for ii in 0..qhei {
+        for jj in 0..qwid {
+          if text_vec[i + ii][j + jj] != query_vec[ii][jj] {
+            found = false;
+            break;
+          }
+        }
+        if !found { break; }
+      }
+      if found {
+        result.push((i, j));
+      }
+    }
+  }
+}
